8f5d23c805378bb20ad273b38a60f81ff658838c
LibraSkywalker <caiwanxin2009@hotmail.com>
2017-08-02 16:44:58 +0000

    first change

--- chatbot.py
+++ chatbot.py
@@ -8,14 +8,17 @@ import time
 fname = time.strftime("%Y-%m-%d") + ".txt"
 @itchat.msg_register([TEXT, SHARING], isGroupChat=True)
 def group_text(msg):
-	f = open(fname,"a+",encoding='gbk')
+	f = open(fname,"a+")
 	dude = bot.List('buddy','菜鸟天堂')
 	content = msg['Text']
-	print(content,file = f)
-	print(content)
+	group = msg['Nickname']
+	if '' in group:
+		print(content,file = f)
+		print(content)
+		print(msg)
 	bot.SendTo(dude[0],content)
 	f.close()
 
-itchat.auto_login(enableCmdQR=True)
-bot.Login(['-q','973231279'])
+itchat.auto_login(enableCmdQR=2)
+bot.Login(['-u','somebody','-q','973231279'])
 itchat.run()
